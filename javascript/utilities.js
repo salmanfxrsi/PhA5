15e5d1c9f4918fadd2da2e6aa4dce249aa08b929
salmanfxrsi <salmanxprivate@gmail.com>
2024-09-25 11:42:30 +0600

    Fixed Validation!

--- javascript/utilities.js
+++ javascript/utilities.js
@@ -1,12 +1,12 @@
 // Get Number Of Input By Using This Function
 function getValueOfInput(id){
-    const value = parseFloat(document.getElementById(id).value);
+    const value = Number(document.getElementById(id).value);
     return value;
 }
 
 // Get InnerText Of Elements By Using This Function
 function numberFromElements(id){
-    const innerText = parseFloat(document.getElementById(id).innerText);
+    const innerText = Number(document.getElementById(id).innerText);
     return innerText;
 }
 
